e66b8e9c615a7b968199721e958536e31d8a2245
Leonardo <leonardoraupprodrigues@hotmail.com>
2024-05-19 18:06:44 -0300

    adicionando pagina inicial

--- src/App.js
+++ src/App.js
@@ -3,22 +3,55 @@ import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import UserForm from "./views/UserForm";
 import UserList from "./views/UserList";
+import { Button, Icon } from "@rneui/base";
 
 const Stack = createNativeStackNavigator();
 
 export default props => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="UserForm">
-                <Stack.Screen
-                    name="UserForm"
-                    component={UserForm}
-                />
+            <Stack.Navigator
+                initialRouteName="UserList"
+                screenOptions={screenOptions}>
                 <Stack.Screen
                     name="UserList"
                     component={UserList}
+                    options={
+                        ({ navigation }) => {
+                            return {
+                                title: "Lista de usuários",
+                                headerRight: () => (
+                                    <Button
+                                        onPress={() => {
+                                            navigation.navigate("UserForm")
+                                        }}
+                                        type="clear"
+                                        icon={<Icon name="add" size={25} color={"white"} />}
+                                    />
+                                )
+                            }
+                        }
+                    }
+
+                />
+                <Stack.Screen
+                    name="UserForm"
+                    component={UserForm}
+                    options={{
+                        title: "Formulário de usuários"
+                    }}
                 />
             </Stack.Navigator>
         </NavigationContainer>
     )
+}
+const screenOptions = {
+    headerStyle: {
+        backgroundColor: '#f4511e'
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+        fontWeight: 'bold'
+    }
+
 }
