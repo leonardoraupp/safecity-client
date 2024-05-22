c0a08ca624d9e4570bd00ed99d5d771444d3cc02
Leonardo <leonardoraupprodrigues@hotmail.com>
2024-05-22 19:55:36 -0300

    adicionando listagem de usuários

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,8 +1,38 @@
 import React from "react";
-import { Text } from "react-native";
+import { View, FlatList } from "react-native";
+import users from '../data/users'
+// import { Avatar, ListItem } from "@rneui/base";
+import { Avatar, ListItem } from '@rneui/themed';
+
 
 export default props => {
+
+    function getUserItem({ item: user }) {
+        return (
+            <ListItem
+                key={user.id}
+                bottomDivider
+                onPress={() => props.navigation.navigate('UserForm')}
+            >
+                <Avatar
+                    size={50}
+                    rounded
+                    source={{ uri: user.avatarUrl }}
+                />
+                <ListItem.Content>
+                    <ListItem.Title>{user.name}</ListItem.Title>
+                    <ListItem.Title>{user.email}</ListItem.Title>
+                </ListItem.Content>
+            </ListItem>
+        )
+    }
     return (
-        <Text>UserList</Text>
+        <View>
+            <FlatList
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserItem}
+            />
+        </View>
     )
 }
