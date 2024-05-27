802e745553858e548a6c01677735978e8bb1beb9
Leonardo <leonardoraupprodrigues@hotmail.com>
2024-05-27 20:36:57 -0300

    adicionando fluxos de edição e exclusão

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,18 +1,52 @@
 import React from "react";
-import { View, FlatList } from "react-native";
+import { View, FlatList, Alert } from "react-native";
 import users from '../data/users'
-// import { Avatar, ListItem } from "@rneui/base";
-import { Avatar, ListItem } from '@rneui/themed';
+import { ListItem, Button, Icon } from "@rneui/base";
+import { Avatar } from '@rneui/themed';
 
 
 export default props => {
+
+    function conformUserDeletion(user) {
+        Alert.alert("Excluir usuário", "Deseja excluir este usuário?", [
+            {
+                text: "Sim",
+                onPress() {
+                    console.warn("delete " + user.id)
+                }
+            },
+            {
+                text: "Não"
+            }
+        ])
+    }
+
+    function getActions(user) {
+        return (
+            <>
+                <Button
+                    onPress={() => { props.navigation.navigate('UserForm', user) }}
+                    type="clear"
+                    icon={<Icon name="edit" size={25} color="orange" />}
+
+
+                />
+                <Button
+                    onPress={() => { conformUserDeletion(user) }}
+                    type="clear"
+                    icon={<Icon name="delete" size={25} color="red" />}
+
+                />
+            </>
+        )
+    }
 
     function getUserItem({ item: user }) {
         return (
             <ListItem
                 key={user.id}
                 bottomDivider
-                onPress={() => props.navigation.navigate('UserForm')}
+                onPress={() => props.navigation.navigate('UserForm', user)}
             >
                 <Avatar
                     size={50}
@@ -21,8 +55,9 @@ export default props => {
                 />
                 <ListItem.Content>
                     <ListItem.Title>{user.name}</ListItem.Title>
-                    <ListItem.Title>{user.email}</ListItem.Title>
+                    <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
                 </ListItem.Content>
+                {getActions(user)}
             </ListItem>
         )
     }
