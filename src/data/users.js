d0efaf68525aa32bb9bc4809f420179686ed5839
Leonardo <leonardoraupprodrigues@hotmail.com>
2024-05-27 20:30:15 -0300

    renomeando usuários

--- src/data/users.js
+++ src/data/users.js
@@ -3,24 +3,30 @@ export default [
         id: 1,
         name: "Leonardo",
         email: "leo@mail.com",
-        avatarUrl: "https://pixabay.com/pt/photos/aluco-coruja-marrom-sentado-7777285/"
+        avatarUrl: "https://cdn.pixabay.com/photo/2023/02/08/18/36/tawny-owl-7777285_960_720.jpg"
     },
     {
         id: 2,
-        name: "Jinny",
-        email: "jinny@mail.com",
-        avatarUrl: "https://pixabay.com/pt/photos/labrador-cachorro-animal-5762115/"
+        name: "Gus",
+        email: "gus@mail.com",
+        avatarUrl: "https://cdn.pixabay.com/photo/2013/07/19/00/18/tiger-165189_640.jpg"
     },
     {
         id: 3,
-        name: "Rafa",
-        email: "rafa@mail.com",
-        avatarUrl: "https://pixabay.com/pt/photos/urso-pardo-urso-predador-ca%C3%A7ador-7597616/"
+        name: "Cris",
+        email: "cris@mail.com",
+        avatarUrl: "https://cdn.pixabay.com/photo/2020/11/20/16/26/labrador-5762115_960_720.jpg"
     },
     {
         id: 4,
-        name: "Cris",
-        email: "cris@mail.com",
-        avatarUrl: "https://pixabay.com/pt/illustrations/ai-gerado-rato-carro-de-corrida-8674235/"
+        name: "Rafa",
+        email: "rafa@mail.com",
+        avatarUrl: "https://cdn.pixabay.com/photo/2022/11/17/08/47/brown-bear-7597616_960_720.jpg"
+    },
+    {
+        id: 5,
+        name: "Renan",
+        email: "renan@mail.com",
+        avatarUrl: "https://cdn.pixabay.com/photo/2020/07/01/17/34/wolf-5360340_960_720.jpg"
     }
 ]
